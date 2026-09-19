434baea07bf7aa35ddcca8c001f52d0258475dfb
studybuddyzone <websiteowner.in@gmail.com>
2026-09-19 16:20:49 +0530

    Add files via upload

---
+++ idcards-data.js
@@ -0,0 +1,141 @@
+// idcards-data.js
+// Shared manifest of every printed Digital ID card: which static
+// id-cards/*.html file it is, and its display name. Read by the Joint
+// Secretary's "ID Cards" photo manager (joint-secretary-workspace.html)
+// and the public idcards.html gallery, so both stay in sync from one
+// place. The "slug" here is exactly the filename (minus .html) and is
+// also the key each card is stored under in the idCardPhotos Realtime
+// Database path (see api/idcard-photos.js).
+
+window.ASTRA_IDCARDS = [
+  {
+    "slug": "member-01-kanishk-tarte",
+    "name": "Kanishk Tarte",
+    "file": "id-cards/member-01-kanishk-tarte.html"
+  },
+  {
+    "slug": "member-02-sanskruti-lokalwar",
+    "name": "Sanskruti Lokalwar",
+    "file": "id-cards/member-02-sanskruti-lokalwar.html"
+  },
+  {
+    "slug": "member-03-anita-dubukwad",
+    "name": "Anita Dubukwad",
+    "file": "id-cards/member-03-anita-dubukwad.html"
+  },
+  {
+    "slug": "member-04-nandkishor-murlidhar-shrimangale",
+    "name": "Nandkishor Murlidhar Shrimangale",
+    "file": "id-cards/member-04-nandkishor-murlidhar-shrimangale.html"
+  },
+  {
+    "slug": "member-05-sanjivani-jadhav",
+    "name": "Sanjivani Jadhav",
+    "file": "id-cards/member-05-sanjivani-jadhav.html"
+  },
+  {
+    "slug": "member-06-pranav-khandare",
+    "name": "Pranav Khandare",
+    "file": "id-cards/member-06-pranav-khandare.html"
+  },
+  {
+    "slug": "member-07-yash-tawde",
+    "name": "Yash Tawde",
+    "file": "id-cards/member-07-yash-tawde.html"
+  },
+  {
+    "slug": "member-08-anurag-pimparne",
+    "name": "Anurag Pimparne",
+    "file": "id-cards/member-08-anurag-pimparne.html"
+  },
+  {
+    "slug": "member-09-shubham-kanhekar",
+    "name": "Shubham Kanhekar",
+    "file": "id-cards/member-09-shubham-kanhekar.html"
+  },
+  {
+    "slug": "member-10-shaikh-aman",
+    "name": "Shaikh Aman",
+    "file": "id-cards/member-10-shaikh-aman.html"
+  },
+  {
+    "slug": "member-11-gitesh-arewar",
+    "name": "Gitesh Arewar",
+    "file": "id-cards/member-11-gitesh-arewar.html"
+  },
+  {
+    "slug": "member-12-shaikh-salik",
+    "name": "Shaikh Salik",
+    "file": "id-cards/member-12-shaikh-salik.html"
+  },
+  {
+    "slug": "member-13-saurabh-lakade",
+    "name": "Saurabh Lakade",
+    "file": "id-cards/member-13-saurabh-lakade.html"
+  },
+  {
+    "slug": "member-14-govind-shelke",
+    "name": "Govind Shelke",
+    "file": "id-cards/member-14-govind-shelke.html"
+  },
+  {
+    "slug": "member-15-ravi-avdhut",
+    "name": "Ravi Avdhut",
+    "file": "id-cards/member-15-ravi-avdhut.html"
+  },
+  {
+    "slug": "member-16-rajeshri-nirdude",
+    "name": "Rajeshri Nirdude",
+    "file": "id-cards/member-16-rajeshri-nirdude.html"
+  },
+  {
+    "slug": "member-17-the-sneha-kadam",
+    "name": "The Sneha Kadam",
+    "file": "id-cards/member-17-the-sneha-kadam.html"
+  },
+  {
+    "slug": "member-18-shraddha-dukare",
+    "name": "Shraddha Dukare",
+    "file": "id-cards/member-18-shraddha-dukare.html"
+  },
+  {
+    "slug": "member-19-shreya-bhosle",
+    "name": "Shreya Bhosle",
+    "file": "id-cards/member-19-shreya-bhosle.html"
+  },
+  {
+    "slug": "member-20-anuja-amol-bhosale",
+    "name": "Anuja Amol Bhosale",
+    "file": "id-cards/member-20-anuja-amol-bhosale.html"
+  },
+  {
+    "slug": "member-21-vaishnavi-bhelonde",
+    "name": "Vaishnavi Bhelonde",
+    "file": "id-cards/member-21-vaishnavi-bhelonde.html"
+  },
+  {
+    "slug": "member-22-megha-gaikwad",
+    "name": "Megha Gaikwad",
+    "file": "id-cards/member-22-megha-gaikwad.html"
+  },
+  {
+    "slug": "member-23-gajanan-lomte",
+    "name": "Gajanan Lomte",
+    "file": "id-cards/member-23-gajanan-lomte.html"
+  },
+  {
+    "slug": "member-24-sohila-shaikh",
+    "name": "Sohila Shaikh",
+    "file": "id-cards/member-24-sohila-shaikh.html"
+  },
+  {
+    "slug": "member-25-sandhya-manohar-dhone",
+    "name": "Sandhya Manohar Dhone",
+    "file": "id-cards/member-25-sandhya-manohar-dhone.html"
+  },
+  {
+    "slug": "member-26-govind-ade",
+    "name": "Govind Ade",
+    "file": "id-cards/member-26-govind-ade.html"
+  }
+];
