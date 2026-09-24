8c3045e1c4d3d060aa339423b349c353a990d519
studybuddyzone <websiteowner.in@gmail.com>
2026-09-24 13:24:15 +0530

    Add files via upload

---
+++ invitations-data.js
@@ -0,0 +1,102 @@
+// invitations-data.js
+// Shared manifest of every Faculty Invitation page: which static
+// faculty-invitations/*.html file it is, and its display name. Mirrors
+// idcards-data.js, read by invitations.html gallery.
+
+window.ASTRA_INVITATIONS = [
+  {
+    "slug": "01_Santosh_Ramdasi",
+    "name": "Santosh Ramdasi",
+    "file": "faculty-invitations/01_Santosh_Ramdasi.html"
+  },
+  {
+    "slug": "02_Gurupreet_Kaur",
+    "name": "Gurupreet Kaur",
+    "file": "faculty-invitations/02_Gurupreet_Kaur.html"
+  },
+  {
+    "slug": "03_Deshmukh_Mam",
+    "name": "Deshmukh Mam",
+    "file": "faculty-invitations/03_Deshmukh_Mam.html"
+  },
+  {
+    "slug": "04_Navin_Kumar_Sir",
+    "name": "Navin Kumar Sir",
+    "file": "faculty-invitations/04_Navin_Kumar_Sir.html"
+  },
+  {
+    "slug": "05_Ullagaddi_Prabhuling_Sir",
+    "name": "Ullagaddi Prabhuling Sir",
+    "file": "faculty-invitations/05_Ullagaddi_Prabhuling_Sir.html"
+  },
+  {
+    "slug": "06_Sanjay_Deolgaonkar_Sir",
+    "name": "Sanjay Deolgaonkar Sir",
+    "file": "faculty-invitations/06_Sanjay_Deolgaonkar_Sir.html"
+  },
+  {
+    "slug": "07_Bhojankar_Mam",
+    "name": "Bhojankar Mam",
+    "file": "faculty-invitations/07_Bhojankar_Mam.html"
+  },
+  {
+    "slug": "08_Wester_Mam",
+    "name": "Wester Mam",
+    "file": "faculty-invitations/08_Wester_Mam.html"
+  },
+  {
+    "slug": "09_Minal_Bhojankar",
+    "name": "Minal Bhojankar",
+    "file": "faculty-invitations/09_Minal_Bhojankar.html"
+  },
+  {
+    "slug": "10_Tahsin_Ali_Sir",
+    "name": "Tahsin Ali Sir",
+    "file": "faculty-invitations/10_Tahsin_Ali_Sir.html"
+  },
+  {
+    "slug": "11_Pathan_Sir",
+    "name": "Pathan Sir",
+    "file": "faculty-invitations/11_Pathan_Sir.html"
+  },
+  {
+    "slug": "12_Vijay_Pawar_Sir",
+    "name": "Vijay Pawar Sir",
+    "file": "faculty-invitations/12_Vijay_Pawar_Sir.html"
+  },
+  {
+    "slug": "13_Pranav_Giri",
+    "name": "Pranav Giri",
+    "file": "faculty-invitations/13_Pranav_Giri.html"
+  },
+  {
+    "slug": "14_Adv_Komal_Wakle_Mam",
+    "name": "Adv. Komal Wakle Mam",
+    "file": "faculty-invitations/14_Adv_Komal_Wakle_Mam.html"
+  },
+  {
+    "slug": "15_Ram_Chauhan_Sir",
+    "name": "Ram Chauhan Sir",
+    "file": "faculty-invitations/15_Ram_Chauhan_Sir.html"
+  },
+  {
+    "slug": "16_Tanishka_Thakur_Mam",
+    "name": "Tanishka Thakur Mam",
+    "file": "faculty-invitations/16_Tanishka_Thakur_Mam.html"
+  },
+  {
+    "slug": "17_Omprakash_Kurulekar_Sir",
+    "name": "Omprakash Kurulekar Sir",
+    "file": "faculty-invitations/17_Omprakash_Kurulekar_Sir.html"
+  },
+  {
+    "slug": "18_Anupama_Lokhande_Mam",
+    "name": "Anupama Lokhande Mam",
+    "file": "faculty-invitations/18_Anupama_Lokhande_Mam.html"
+  },
+  {
+    "slug": "19_Pedge_Sir",
+    "name": "Pedge Sir",
+    "file": "faculty-invitations/19_Pedge_Sir.html"
+  }
+];
